2999d797c430889de5dfd03283fbd7a4235865a4
Aman Srivastava <88317324+amanonearth@users.noreply.github.com>
2022-11-24 02:12:02 +0530

    bash script not needed anymore

--- NFT Engine/test.sh
+++
@@ -1,5 +0,0 @@
-curl --request POST \
-  --url https://api-eu1.tatum.io/v3/ipfs \
-  --header 'content-type: multipart/form-data' \
-  --header 'x-api-key: a2c55403-d269-4703-89f9-2558005bdbde' \
-  -F upload=@"/Users/a-manonearth/Sites/ST/NFT/output.jpg"
